118d261f4601cbcfa5826a83a7b6e5c6a2587cf4
bladimirhurtado <bladimirhurtado21669@gmail.com>
2026-09-19 15:57:39 -0400

    chore: refresh Bladi 3AI offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bladi3ai-2026-09-10-v3';
+const CACHE='bladi3ai-2026-09-19-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
